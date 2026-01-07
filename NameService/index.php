803534dde8bca7af16f9b8165765dda6ffbf5573
Nicoco92 <nicolas.contreras-tibocha@efrei.net>
2026-01-07 11:01:03 +0100

    Ajout docker compose

--- NameService/index.php
+++ NameService/index.php
@@ -1,6 +1,14 @@
 <?php
-
 header('Content-Type: text/plain');
-echo "Nicolas"; 
 
+
+$name = $_GET['name'] ?? 'Nicolas';
+
+$name = trim($name);
+
+if ($name === "") {
+    echo json_encode(["error" => "Paramètre 'name' vide"]);
+} else {
+    echo $name;
+}
 ?>
--- RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
+++ RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
@@ -1,13 +1,22 @@
 package com.ingnum.rentalservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class BonjourController {
 
+    @Value("${nameservice.url}")
+    private String nameServiceUrl;
+
     @GetMapping("/bonjour")
     public String bonjour() {
-        return "bonjour";
+        RestTemplate restTemplate = new RestTemplate();
+        
+        String name = restTemplate.getForObject(nameServiceUrl, String.class);
+        
+        return "Bonjour " + name;
     }
 }
